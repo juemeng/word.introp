7809c33ac793aa46e944d8934cccb59ccdb5a55b
Jo <zoul@shinetechchina.com>
2016-10-23 12:37:59 +0800

    add tiff helper

---
+++ ConsoleApplication1/TiffHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    public class TiffHelper
+    {
+        public static void MergeTiffImages(string distFile, params string[] tiffFiles)
+        {
+            var codecInfo = ImageCodecInfo.GetImageEncoders()[3];
+            var saveEncoder = Encoder.SaveFlag;
+            var compressionEncoder = Encoder.Compression;
+            var saveEncodeParam = new EncoderParameter(saveEncoder, (long)EncoderValue.MultiFrame);
+            var compressionEncodeParam = new EncoderParameter(compressionEncoder, (long)EncoderValue.CompressionLZW);
+            var encoderParams = new EncoderParameters(2)
+            {
+                Param =
+                    {
+                        [0] = saveEncodeParam,
+                        [1] = compressionEncodeParam
+                    }
+            };
+
+
+            Bitmap pages = null;
+            var frame = 0;
+            foreach (var strImageFile in tiffFiles)
+            {
+                if (frame == 0)
+                {
+                    pages = (Bitmap)Image.FromFile(strImageFile);
+                    pages.Save(distFile, codecInfo, encoderParams);
+                }
+                else
+                {
+                    encoderParams.Param[0] = new EncoderParameter(saveEncoder, (long)EncoderValue.FrameDimensionPage);
+                    var bm = (Bitmap)Image.FromFile(strImageFile);
+                    pages.SaveAdd(bm, encoderParams);
+                    bm.Dispose();
+                }
+
+                if (frame == tiffFiles.Length - 1)
+                {
+                    encoderParams.Param[0] = new EncoderParameter(saveEncoder, (long)EncoderValue.Flush);
+                    pages.SaveAdd(encoderParams);
+                }
+                frame++;
+            }
+
+            pages.Dispose(); 
+        }
+    }
+}
